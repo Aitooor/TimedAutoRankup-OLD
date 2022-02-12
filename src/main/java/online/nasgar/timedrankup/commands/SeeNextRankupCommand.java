16ffbb77f186385ae2162993f91274bb00c278d5
pixeldev <pixeldev1729@gmail.com>
2022-02-12 09:41:39 -0600

    feat(translation): use nmessage

--- src/main/java/online/nasgar/timedrankup/commands/SeeNextRankupCommand.java
+++ src/main/java/online/nasgar/timedrankup/commands/SeeNextRankupCommand.java
@@ -1,12 +1,10 @@
 package online.nasgar.timedrankup.commands;
 
 import lombok.RequiredArgsConstructor;
-import me.clip.placeholderapi.PlaceholderAPI;
 import online.nasgar.timedrankup.TimedRankup;
 import online.nasgar.timedrankup.rank.Rank;
 import online.nasgar.timedrankup.user.User;
 import online.nasgar.timedrankup.utils.TimeUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -34,15 +32,16 @@ public class SeeNextRankupCommand implements CommandExecutor {
 
             Rank rank = timedRankup.getRankManager().getNextApplicable(user);
             if (rank != null) {
-                timedRankup.getConfig().getStringList("commands.see.next").forEach(s -> {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, s
-                            .replaceAll("%new_rank_prefix%", rank.getPrefix())
-                            .replaceAll("%parsedTime%", TimeUtils.formatTime(Duration.ofSeconds(rank.getTime()-user.getTime().get()))))
-                    ));
-                });
+                timedRankup.getMessageHandler().sendReplacingIn(
+                        player, "placeholder",
+                        "commands.see.next",
+                        "%new_rank_prefix%", rank.getPrefix(),
+                        "%parsedTime%", TimeUtils.formatTime(
+                                Duration.ofSeconds(rank.getTime() - user.getTime().get())
+                        )
+                );
             } else {
-                timedRankup.getConfig().getStringList("commands.see.noMore").forEach(s -> player.sendMessage(ChatColor.translateAlternateColorCodes('&', s)));
-
+                timedRankup.getMessageHandler().send(player, "commands.see.noMore");
             }
         }
         return true;
